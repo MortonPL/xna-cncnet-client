1709f80f741150a8e11148c95ae8484d7a842c18
MortonPL <bartm12@wp.pl>
2023-10-20 23:21:50 +0200

    Make CheaterScreen INItializable

--- DXMainClient/DXGUI/Generic/CheaterWindow.cs
+++ DXMainClient/DXGUI/Generic/CheaterWindow.cs
@@ -7,7 +7,7 @@ using ClientCore.Extensions;
 
 namespace DTAClient.DXGUI.Generic
 {
-    public class CheaterWindow : XNAWindow
+    public class CheaterWindow : INItializableWindow
     {
         public CheaterWindow(WindowManager windowManager) : base(windowManager)
         {
@@ -21,52 +21,15 @@ namespace DTAClient.DXGUI.Generic
             ClientRectangle = new Rectangle(0, 0, 334, 453);
             BackgroundTexture = AssetLoader.LoadTexture("cheaterbg.png");
 
-            var lblCheater = new XNALabel(WindowManager);
-            lblCheater.Name = "lblCheater";
-            lblCheater.ClientRectangle = new Rectangle(0, 0, 0, 0);
-            lblCheater.FontIndex = 1;
-            lblCheater.Text = "CHEATER!".L10N("Client:Main:Cheater");
+            base.Initialize();
 
-            var lblDescription = new XNALabel(WindowManager);
-            lblDescription.Name = "lblDescription";
-            lblDescription.ClientRectangle = new Rectangle(12, 40, 0, 0);
-            lblDescription.Text = ("Modified game files have been detected. They could affect\n" +
-                "the game experience.\n\n" +
-                "Do you really lack the skill for winning the mission without\ncheating?").L10N("Client:Main:CheaterText");
-
-            var imagePanel = new XNAPanel(WindowManager);
-            imagePanel.Name = "imagePanel";
-            imagePanel.PanelBackgroundDrawMode = PanelBackgroundImageDrawMode.STRETCHED;
-            imagePanel.ClientRectangle = new Rectangle(lblDescription.X,
-                lblDescription.Bottom + 12, Width - 24,
-                Height - (lblDescription.Bottom + 59));
-            imagePanel.BackgroundTexture = AssetLoader.LoadTextureUncached("cheater.png");
-
-            var btnCancel = new XNAClientButton(WindowManager);
-            btnCancel.Name = "btnCancel";
-            btnCancel.ClientRectangle = new Rectangle(Width - 104,
-                Height - 35, UIDesignConstants.BUTTON_WIDTH_92, UIDesignConstants.BUTTON_HEIGHT);
-            btnCancel.Text = "Cancel".L10N("Client:Main:ButtonCancel");
+            XNAClientButton btnCancel;
+            btnCancel = FindChild<XNAClientButton>(nameof(btnCancel));
             btnCancel.LeftClick += BtnCancel_LeftClick;
 
-            var btnYes = new XNAClientButton(WindowManager);
-            btnYes.Name = "btnYes";
-            btnYes.ClientRectangle = new Rectangle(12, btnCancel.Y,
-                btnCancel.Width, btnCancel.Height);
-            btnYes.Text = "Yes".L10N("Client:Main:ButtonYes");
+            XNAClientButton btnYes;
+            btnYes = FindChild<XNAClientButton>(nameof(btnYes));
             btnYes.LeftClick += BtnYes_LeftClick;
-
-            AddChild(lblCheater);
-            AddChild(lblDescription);
-            AddChild(imagePanel);
-            AddChild(btnCancel);
-            AddChild(btnYes);
-
-            lblCheater.CenterOnParent();
-            lblCheater.ClientRectangle = new Rectangle(lblCheater.X, 12,
-                lblCheater.Width, lblCheater.Height);
-
-            base.Initialize();
         }
 
         private void BtnCancel_LeftClick(object sender, EventArgs e)
